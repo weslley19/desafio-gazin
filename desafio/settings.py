8be62007cb68d6515a4da4cb72d7e51536f040e7
Weslley Oliveira <weslleyoliveira258@gmail.com>
2021-11-01 22:53:30 -0300

    feat: deploy heroku

--- desafio/settings.py
+++ desafio/settings.py
@@ -153,6 +153,17 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_URL = '/static/'
+if DEBUG:
+   STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, 'static'),
+   ]
+else:
+   STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
